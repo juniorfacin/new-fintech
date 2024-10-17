b79852e946d5d82728fe5630aa527127feb86c4d
JoaoPedroFazzolo <143749763+JoaoPedroFazzolo@users.noreply.github.com>
2024-10-16 21:06:08 -0300

    Acerto no SQL

--- src/main/java/br/com/moneyiteasy/service/transaction/ExpenseManager.java
+++ src/main/java/br/com/moneyiteasy/service/transaction/ExpenseManager.java
@@ -1,12 +1,14 @@
 package br.com.moneyiteasy.service.transaction;
 
+import br.com.moneyiteasy.dao.ExpenseDao;
 import br.com.moneyiteasy.model.transaction.Expense;
 import br.com.moneyiteasy.model.transaction.Transaction;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ExpenseManager extends TransactionManager {
-
+    ExpenseDao expenseDao = new ExpenseDao();
     @Override
     protected String getTransactionType() {
         return "despesa";
@@ -15,6 +17,20 @@ public class ExpenseManager extends TransactionManager {
     @Override
     protected Transaction createTransaction(String category, double value, LocalDateTime timestamp, String method) {
         return new Expense(category, value, timestamp, method, false);
+    }
+
+    public void displayTransactions() {
+        List<Expense> expenses = expenseDao.getAllExpenses(); // Usando o DAO para buscar despesas
+        if (expenses.isEmpty()) {
+            System.out.println("Nenhuma despesa cadastrada.");
+        } else {
+            System.out.println("Despesas cadastradas:");
+            for (Expense expense : expenses) {
+                System.out.printf("Categoria: %s | Valor: R$ %.2f | Data e Hora: %s | Método de pagamento: %s\n",
+                        expense.getCategory(), expense.getValue(), expense.getFormattedTimestamp(), expense.getMethod());
+            }
+            System.out.printf("Total de despesas: R$ %.2f\n", getTotalValue());
+        }
     }
 
 
